4023b83edfc073e1302abb32990e2e0b61d87091
Raziyeh Bazargan <Raziyeh.bazargan@gmail.com>
2018-09-04 15:44:30 -0700

    Initial Build

--- Portfolio/App_Start/BundleConfig.cs
+++ Portfolio/App_Start/BundleConfig.cs
@@ -24,7 +24,7 @@ namespace Portfolio
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
+                      "~/Content/Site.css"));
         }
     }
 }
